f45a6cea078454c8c235113ce4e370eae277f358
Dor <dorhakim100@gmail.com>
2025-08-16 08:41:41 +0300

    Add basic circular slider

--- src/components/CaloriesProgress/CaloriesProgress.tsx
+++ src/components/CaloriesProgress/CaloriesProgress.tsx
@@ -1,4 +1,5 @@
-import { Card, Typography } from '@mui/material'
+import { Card, Typography, Box, Stack, Button } from '@mui/material'
+// import {Slider} from '@mui/material'
 import { CircularProgress } from '../CircularProgress/CircularProgress'
 
 import { useSelector } from 'react-redux'
@@ -73,6 +74,81 @@ export function CaloriesProgress({
   )
 }
 
+import Slider from 'rc-slider'
+// import CircularSlider from '@fseehawer/react-circular-slider'
+import CircularSlider from 'react-circular-slider-svg'
+
 function EditComponent() {
-  return <div>EditComponent</div>
+  const [calories, setCalories] = useState<number>(2000)
+
+  const marks = [
+    { value: 0, label: '0' },
+    { value: 1000, label: '1k' },
+    { value: 2000, label: '2k' },
+    { value: 3000, label: '3k' },
+    { value: 4000, label: '4k' },
+  ]
+
+  const handleChange = (value: number) => {
+    const valueToSet = Math.round(value / 50) * 50
+
+    setCalories(valueToSet)
+  }
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation()
+    e.preventDefault()
+    console.log('clicked')
+    // setCalories(1600)
+  }
+
+  return (
+    <Box sx={{ p: 2 }}>
+      <Box
+        sx={{
+          position: 'relative',
+          // height: 200,
+        }}
+        className='calories-amount-container'
+      >
+        <Typography
+          variant='h3'
+          onClick={handleClick}
+          className='calories-amount'
+        >
+          {calories} kcal
+        </Typography>
+        <div className='circular-slider-container'>
+          <CircularSlider
+            size={300}
+            trackWidth={12}
+            minValue={1600}
+            maxValue={4000}
+            startAngle={180} // start at left
+            endAngle={360} // end at right (half circle)
+            handle1={{
+              value: calories,
+              onChange: handleChange,
+            }}
+            arcColor='#009688'
+            arcBackgroundColor='#3A3F47'
+          />
+        </div>
+      </Box>
+      {/* <Stack direction='row' spacing={1} justifyContent='center'>
+        <Button
+          variant='outlined'
+          onClick={() => setCalories((v) => Math.max(0, v - 50))}
+        >
+          -50
+        </Button>
+        <Button
+          variant='outlined'
+          onClick={() => setCalories((v) => Math.min(4000, v + 50))}
+        >
+          +50
+        </Button>
+      </Stack> */}
+    </Box>
+  )
 }
