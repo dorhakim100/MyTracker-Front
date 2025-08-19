d5ff3c46159f0b09920f54688bca435394e4524a
Dor <dorhakim100@gmail.com>
2025-08-19 19:14:09 +0300

    Add toggle button options, search source

--- src/components/ItemSearch/ItemSearch.tsx
+++ src/components/ItemSearch/ItemSearch.tsx
@@ -16,6 +16,8 @@ import { useSelector } from 'react-redux'
 import { searchService } from '../../services/search/search-service'
 
 import { RootState } from '../../store/store'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
+import ToggleButton from '@mui/material/ToggleButton'
 
 export function ItemSearch() {
   const prefs = useSelector((state: RootState) => state.systemModule.prefs)
@@ -23,6 +25,8 @@ export function ItemSearch() {
   const [query, setQuery] = useState('')
   const [results, setResults] = useState<any[]>([])
   const [loading, setLoading] = useState(false)
+
+  const [source, setSource] = useState('usda')
 
   useEffect(() => {
     if (query) {
@@ -39,6 +43,13 @@ export function ItemSearch() {
 
   const onClose = () => {
     console.log('onClose')
+  }
+
+  const handleSource = (
+    event: React.MouseEvent<HTMLElement>,
+    newSource: string
+  ) => {
+    setSource(newSource)
   }
 
   return (
@@ -66,6 +77,20 @@ export function ItemSearch() {
             ) : undefined,
           }}
         />
+        <ToggleButtonGroup
+          value={source}
+          exclusive
+          onChange={handleSource}
+          aria-label='text alignment'
+          className={`source-toggle ${prefs.isDarkMode ? 'dark-mode' : ''}`}
+        >
+          <ToggleButton value='usda' aria-label='centered'>
+            Food
+          </ToggleButton>
+          <ToggleButton value='open-food-facts' aria-label='left aligned'>
+            Product
+          </ToggleButton>
+        </ToggleButtonGroup>
       </Box>
 
       <Box className='results'>
