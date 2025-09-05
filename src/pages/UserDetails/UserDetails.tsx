ab0d62f682b6b15134b1a2009a4ca54a7c46a6e2
Dor <dorhakim100@gmail.com>
2025-09-05 16:34:04 +0300

    Add favorite color support based on prefs

--- src/pages/UserDetails/UserDetails.tsx
+++ src/pages/UserDetails/UserDetails.tsx
@@ -1,11 +1,26 @@
 import { useMemo, useState } from 'react'
 import { useSelector } from 'react-redux'
-import { Button, Card, CardContent, Divider, Typography } from '@mui/material'
+import { Button, Card, Divider, IconButton, Typography } from '@mui/material'
 import { RootState } from '../../store/store'
 import { logout } from '../../store/actions/user.actions'
 import { setPrefs } from '../../store/actions/system.actions'
 import type { Prefs } from '../../types/system/Prefs'
 import { DarkModeSwitch } from '../../components/DarkModeSwitch/DarkModeSwitch'
+import CheckIcon from '@mui/icons-material/Check'
+
+import { motion } from 'framer-motion'
+
+const colors = {
+  primary: '--var(--primary-color)',
+  blue: '--var(--picker-color-blue)',
+  yellow: '--var(--picker-color-yellow)',
+  red: '--var(--picker-color-red)',
+  orange: '--var(--picker-color-orange)',
+  green: '--var(--picker-color-green)',
+  deepPurple: '--var(--picker-color-deep-purple)',
+  purple: '--var(--picker-color-purple)',
+  pink: '--var(--picker-color-pink)',
+}
 
 export function UserDetails() {
   const prefs = useSelector(
@@ -18,10 +33,7 @@ export function UserDetails() {
     prefs.favoriteColor || '#1976d2'
   )
 
-  const colorChoices = useMemo<string[]>(
-    () => ['#1976d2', '#9c27b0', '#2e7d32', '#ed6c02', '#d32f2f', '#00897b'],
-    []
-  )
+  const colorChoices = useMemo<string[]>(() => Object.keys(colors), [])
 
   function onToggleDarkMode() {
     const newPrefs: Prefs = { ...prefs, isDarkMode: !prefs.isDarkMode }
@@ -29,6 +41,7 @@ export function UserDetails() {
   }
 
   function onChangeFavoriteColor(color: string) {
+    console.log('color', color)
     setFavoriteColor(color)
     const newPrefs: Prefs = { ...prefs, favoriteColor: color }
     setPrefs(newPrefs)
@@ -81,21 +94,46 @@ export function UserDetails() {
           <div className='color-options'>
             {colorChoices.map((color) => (
               <Button
-                key={color}
-                className={`color-button ${
+                key={`${color}-color-button`}
+                className={`color-button ${color} ${
                   favoriteColor === color ? 'selected' : ''
                 }`}
                 onClick={() => onChangeFavoriteColor(color)}
-                data-color={color}
-              />
+              >
+                <ColorMotion color={color} favoriteColor={favoriteColor} />
+              </Button>
             ))}
           </div>
         </div>
       </Card>
 
-      <Button variant='contained' color='primary' onClick={() => logout()}>
+      <Button
+        variant='contained'
+        color='primary'
+        onClick={() => logout()}
+        className={`${prefs.favoriteColor}`}
+      >
         Logout
       </Button>
     </div>
+  )
+}
+
+function ColorMotion({
+  color,
+  favoriteColor,
+}: {
+  color: string
+  favoriteColor: string
+}) {
+  return (
+    <motion.div
+      whileTap={{ scale: 0.8 }}
+      animate={color === favoriteColor ? { scale: [1, 1.3, 1] } : { scale: 1 }}
+      transition={{ duration: 0.3 }}
+      className='motion-container'
+    >
+      {color === favoriteColor ? <CheckIcon /> : null}
+    </motion.div>
   )
 }
