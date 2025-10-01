9172d6c464f6c3bc9241c1c3ded8bb4ca43fabe3
Dor <dorhakim100@gmail.com>
2025-10-01 16:16:21 +0300

    Add basic CustomStepper to EditGoal

--- src/components/EditGoal/EditGoal.tsx
+++ src/components/EditGoal/EditGoal.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Goal } from '../../types/goal/Goal'
+import CustomStepper from '../../CustomMui/CustomStepper/CustomStepper'
 
 interface EditGoalProps {
   selectedGoal?: Goal | null
   saveGoal: (goal: Goal) => void
 }
 
+const stages = ['title', 'target', 'macros']
+
 export function EditGoal({ selectedGoal, saveGoal }: EditGoalProps) {
-  return <div>EditGoal</div>
+  const [activeStage, setActiveStage] = useState<string>(stages[0])
+
+  const onStageChange = (targetStage: string, diff: number) => {
+    setActiveStage(targetStage)
+  }
+
+  const renderStage = () => {
+    switch (activeStage) {
+      case 'title':
+        return <span>Title</span>
+      case 'target':
+        return <span>Target</span>
+      case 'macros':
+        return <span>Macros</span>
+    }
+  }
+
+  return (
+    <div className='edit-goal-container'>
+      <CustomStepper
+        stages={stages}
+        activeStage={activeStage}
+        onStageChange={onStageChange}
+        renderStage={renderStage}
+      />
+    </div>
+  )
 }
