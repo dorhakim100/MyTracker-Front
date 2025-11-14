d2e689732f28b181ca8dda9e4ca66d3b826707f3
Dor Hakim <dorhakim100@gmail.com>
2025-11-14 15:21:56 +0200

    Add app to Route interface

--- src/assets/config/apps.ts
+++ src/assets/config/apps.ts
@@ -1,15 +1,16 @@
 import myTrackerLogo from '../../../public/logo-square.png'
 import liftMateLogo from '../../../public/lift-mate-logo-square.png'
+import { App } from '../../types/app/App'
 
 export const apps = {
   myTracker: {
     name: 'My Tracker',
     logo: myTrackerLogo,
-    id: 'my-tracker',
+    id: 'my-tracker' as App,
   },
   liftMate: {
     name: 'Lift Mate',
     logo: liftMateLogo,
-    id: 'lift-mate',
+    id: 'lift-mate' as App,
   },
 }
--- src/assets/routes/routes.ts
+++ src/assets/routes/routes.ts
@@ -7,12 +7,14 @@ import { Progress } from '../../pages/Progress/Progress'
 import { UserDetails } from '../../pages/UserDetails/UserDetails'
 
 import { SignIn } from '../../CustomMui/SignIn/SignIn.tsx'
+import { App } from '../../types/app/App'
 
 export interface Route {
   title: string
   path: string
   element: React.ComponentType
   icon: React.ComponentType
+  app: App
 }
 
 import DashboardIcon from '@mui/icons-material/Dashboard'
@@ -20,6 +22,7 @@ import FormatListBulletedAddIcon from '@mui/icons-material/FormatListBulletedAdd
 import InsightsIcon from '@mui/icons-material/Insights'
 import SignInIcon from '@mui/icons-material/Login'
 import UserIcon from '@mui/icons-material/Person'
+import { apps } from '../config/apps.ts'
 
 export const routes: Route[] = [
   {
@@ -27,18 +30,21 @@ export const routes: Route[] = [
     path: '/',
     element: Dashboard,
     icon: DashboardIcon,
+    app: apps.myTracker.id,
   },
   {
     title: 'Diary',
     path: '/diary',
     element: Diary,
     icon: FormatListBulletedAddIcon,
+    app: apps.myTracker.id,
   },
   {
     title: 'Progress',
     path: '/progress',
     element: Progress,
     icon: InsightsIcon,
+    app: apps.myTracker.id,
   },
 
   {
@@ -46,6 +52,7 @@ export const routes: Route[] = [
     path: '/signin',
     element: SignIn,
     icon: SignInIcon,
+    app: apps.myTracker.id,
   },
 
   {
@@ -53,5 +60,6 @@ export const routes: Route[] = [
     path: '/user',
     element: UserDetails,
     icon: UserIcon,
+    app: apps.myTracker.id,
   },
 ]
