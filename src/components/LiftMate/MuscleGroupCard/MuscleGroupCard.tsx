eb94e8235bb1d8a5ed58e6522762955f2c2ef54f
Dor Hakim <dorhakim100@gmail.com>
2025-11-19 14:01:49 +0200

    Add styling to muscle group card to position overlay

--- src/components/LiftMate/MuscleGroupCard/MuscleGroupCard.tsx
+++ src/components/LiftMate/MuscleGroupCard/MuscleGroupCard.tsx
@@ -1,18 +1,39 @@
+import { useSelector } from 'react-redux'
 import { MuscleGroup } from '../../../types/muscleGroup/MuscleGroup'
+
+import { Card } from '@mui/material'
+import { RootState } from '../../../store/store'
 
 const BASE_FRONT =
   'https://wger.de/static/images/muscles/muscular_system_front.svg'
 
 const BASE_BACK =
   'https://wger.de/static/images/muscles/muscular_system_back.svg'
-
 export function MuscleGroupCard({ muscleGroup }: { muscleGroup: MuscleGroup }) {
+  const prefs = useSelector(
+    (stateSelector: RootState) => stateSelector.systemModule.prefs
+  )
   const getBase = () => {
     return muscleGroup.base === 'front' ? BASE_FRONT : BASE_BACK
   }
 
+  const getMuscleContainerClass = () => {
+    const group = muscleGroup.name
+    if (group === 'calves' || group === 'hamstrings') {
+      return `lower-body ${group}`
+    }
+    if (group === 'abs' || group === 'quads' || group === 'glutes')
+      return `middle ${group}`
+
+    return `upper-body ${group}`
+  }
+
   return (
-    <div className="muscle-container">
+    <Card
+      className={`card muscle-container ${
+        prefs.isDarkMode ? 'dark-mode' : ''
+      } ${getMuscleContainerClass()}`}
+    >
       <img src={getBase()} alt="base" className="base-img" />
       {muscleGroup.img.map((img) => (
         <img
@@ -22,6 +43,6 @@ export function MuscleGroupCard({ muscleGroup }: { muscleGroup: MuscleGroup }) {
           className="muscle-overlay"
         />
       ))}
-    </div>
+    </Card>
   )
 }
