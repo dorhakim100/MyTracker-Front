557cfa9e221bfd60d72c6d1db48f3a9dbd90e6e5
Dor Hakim <dorhakim100@gmail.com>
2025-11-22 13:42:29 +0200

    Move renderWorkout to it's onw cmp called WorkoutSession

---
+++ src/components/WorkoutSession/WorkoutSession.tsx
@@ -0,0 +1,101 @@
+import { useCallback, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Typography } from '@mui/material'
+
+import { RootState } from '../../store/store'
+import { SessionDay } from '../../types/workout/SessionDay'
+import { Exercise } from '../../types/exercise/Exercise'
+import { capitalizeFirstLetter } from '../../services/util.service'
+import { CustomAccordion } from '../../CustomMui/CustomAccordion/CustomAccordion'
+import { CustomButton } from '../../CustomMui/CustomButton/CustomButton'
+import InfoOutlineIcon from '@mui/icons-material/InfoOutline'
+import ExpandLessIcon from '@mui/icons-material/ExpandLess'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+interface WorkoutSessionProps {
+  sessionDay: SessionDay
+  onExerciseInfoClick: (exercise: Exercise) => void
+}
+
+export function WorkoutSession({
+  sessionDay,
+  onExerciseInfoClick,
+}: WorkoutSessionProps) {
+  const prefs = useSelector((state: RootState) => state.systemModule.prefs)
+
+  const [expandedExercises, setExpandedExercises] = useState<Set<string>>(
+    new Set()
+  )
+
+  const handleAccordionChange = useCallback(
+    (exerciseId: string) =>
+      (_event: React.SyntheticEvent, isExpanded: boolean) => {
+        setExpandedExercises((prev) => {
+          const newSet = new Set(prev)
+          if (isExpanded) {
+            newSet.add(exerciseId)
+          } else {
+            newSet.delete(exerciseId)
+          }
+          return newSet
+        })
+      },
+    []
+  )
+
+  const toggleExpandAll = () => {
+    if (!sessionDay?.workout?.exercises) return
+    const allExerciseIds = sessionDay.workout.exercises.map(
+      (ex) => ex.exerciseId
+    )
+    const allExpanded = allExerciseIds.every((id) => expandedExercises.has(id))
+
+    if (allExpanded) {
+      setExpandedExercises(new Set())
+    } else {
+      setExpandedExercises(new Set(allExerciseIds))
+    }
+  }
+
+  const allExerciseIds = sessionDay.workout.exercises.map((ex) => ex.exerciseId)
+  const allExpanded =
+    allExerciseIds.length > 0 &&
+    allExerciseIds.every((id) => expandedExercises.has(id))
+
+  return (
+    <div className='workout-container'>
+      <div className='workout-header-container'>
+        <Typography variant='h5' className='bold-header'>
+          {sessionDay?.workout?.name}
+        </Typography>
+        <CustomButton
+          icon={allExpanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+          onClick={toggleExpandAll}
+        />
+      </div>
+      <div className='exercises-container'>
+        {sessionDay.workout.exercises.map((exercise) => {
+          const isExpanded = expandedExercises.has(exercise.exerciseId)
+
+          return (
+            <CustomAccordion
+              key={`${exercise.exerciseId}-${sessionDay._id}`}
+              title={capitalizeFirstLetter(exercise.name)}
+              cmp={<div>bla</div>}
+              expanded={isExpanded}
+              onChange={handleAccordionChange(exercise.exerciseId)}
+              icon={
+                <InfoOutlineIcon
+                  onClick={(ev) => {
+                    ev.stopPropagation()
+                    onExerciseInfoClick(exercise)
+                  }}
+                />
+              }
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
--- src/pages/LiftMate/Session/Session.tsx
+++ src/pages/LiftMate/Session/Session.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../../store/store'
 
@@ -26,13 +26,8 @@ import { Exercise } from '../../../types/exercise/Exercise'
 import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite'
 import { setIsLoading } from '../../../store/actions/system.actions'
 import { SlideAnimation } from '../../../components/SlideAnimation/SlideAnimation'
-import { CustomAccordion } from '../../../CustomMui/CustomAccordion/CustomAccordion'
-import InfoOutlineIcon from '@mui/icons-material/InfoOutline'
 import { ExerciseDetails } from '../../../components/ExerciseDetails/ExerciseDetails'
-
-import ExpandLessIcon from '@mui/icons-material/ExpandLess'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import { CustomButton } from '../../../CustomMui/CustomButton/CustomButton'
+import { WorkoutSession } from '../../../components/WorkoutSession/WorkoutSession'
 
 const WORKOUT = 'workout'
 const EXERCISE = 'exercise'
@@ -71,10 +66,6 @@ export function Session() {
     item: null,
     type: null,
   })
-
-  const [expandedExercises, setExpandedExercises] = useState<Set<string>>(
-    new Set()
-  )
 
   const isToday = useMemo(() => {
     const isToday =
@@ -123,7 +114,6 @@ export function Session() {
   }
 
   const onStartWorkout = async (workout: Workout) => {
-    console.log('workout', workout)
     if (!workout._id || !sessionDay) return
     try {
       setIsLoading(true)
@@ -171,87 +161,12 @@ export function Session() {
     )
   }
 
-  const handleAccordionChange = useCallback(
-    (exerciseId: string) =>
-      (_event: React.SyntheticEvent, isExpanded: boolean) => {
-        setExpandedExercises((prev) => {
-          const newSet = new Set(prev)
-          if (isExpanded) {
-            newSet.add(exerciseId)
-          } else {
-            newSet.delete(exerciseId)
-          }
-          return newSet
-        })
-      },
-    []
-  )
-
-  const toggleExpandAll = () => {
-    if (!sessionDay?.workout?.exercises) return
-    const allExerciseIds = sessionDay.workout.exercises.map(
-      (ex) => ex.exerciseId
-    )
-    const allExpanded = allExerciseIds.every((id) => expandedExercises.has(id))
-
-    if (allExpanded) {
-      setExpandedExercises(new Set())
-    } else {
-      setExpandedExercises(new Set(allExerciseIds))
-    }
-  }
-
-  const renderWorkout = () => {
-    console.log('sessionDay', sessionDay)
-    if (!sessionDay) return null
-
-    const allExerciseIds = sessionDay.workout.exercises.map(
-      (ex) => ex.exerciseId
-    )
-    const allExpanded =
-      allExerciseIds.length > 0 &&
-      allExerciseIds.every((id) => expandedExercises.has(id))
-
-    return (
-      <div className='workout-container'>
-        <div className='workout-header-container'>
-          <Typography variant='h5' className='bold-header'>
-            {sessionDay?.workout?.name}
-          </Typography>
-          <CustomButton
-            icon={allExpanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-            onClick={toggleExpandAll}
-          />
-        </div>
-        <div className='exercises-container'>
-          {sessionDay.workout.exercises.map((exercise) => {
-            const isExpanded = expandedExercises.has(exercise.exerciseId)
-
-            return (
-              <CustomAccordion
-                key={`${exercise.exerciseId}-${sessionDay._id}`}
-                title={capitalizeFirstLetter(exercise.name)}
-                cmp={<div>bla</div>}
-                expanded={isExpanded}
-                onChange={handleAccordionChange(exercise.exerciseId)}
-                icon={
-                  <InfoOutlineIcon
-                    onClick={(ev) => {
-                      ev.stopPropagation()
-                      setDialogOptions({
-                        open: true,
-                        item: exercise,
-                        type: EXERCISE,
-                      })
-                    }}
-                  />
-                }
-              />
-            )
-          })}
-        </div>
-      </div>
-    )
+  const handleExerciseInfoClick = (exercise: Exercise) => {
+    setDialogOptions({
+      open: true,
+      item: exercise,
+      type: EXERCISE,
+    })
   }
 
   const getDialogComponent = () => {
@@ -277,7 +192,14 @@ export function Session() {
           onDateChange={onDateChange}
         />
         <SlideAnimation motionKey={sessionDay._id} direction={direction}>
-          {!sessionDay.workoutId ? renderNoSession() : renderWorkout()}
+          {!sessionDay.workoutId ? (
+            renderNoSession()
+          ) : (
+            <WorkoutSession
+              sessionDay={sessionDay}
+              onExerciseInfoClick={handleExerciseInfoClick}
+            />
+          )}
         </SlideAnimation>
       </div>
       {dialogOptions.type && (
