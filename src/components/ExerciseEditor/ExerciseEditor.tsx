3977f3e6ec0ca49681753a227748b6d2d87daefe
Dor Hakim <dorhakim100@gmail.com>
2025-11-23 11:38:14 +0200

    Add basic PickerSelect to ExerciseEditor

--- src/components/ExerciseEditor/ExerciseEditor.tsx
+++ src/components/ExerciseEditor/ExerciseEditor.tsx
@@ -10,20 +10,23 @@ import { useSelector } from 'react-redux'
 import { showErrorMsg } from '../../services/event-bus.service'
 import { messages } from '../../assets/config/messages'
 import { saveSessionDay } from '../../store/actions/workout.action'
+import { Divider } from '@mui/material'
+import { PickerSelect } from '../Pickers/PickerSelect'
 
 export interface ExerciseEditorProps {
   exercise: Exercise
 }
 
 export function ExerciseEditor({ exercise }: ExerciseEditorProps) {
+  const prefs = useSelector(
+    (stateSelector: RootState) => stateSelector.systemModule.prefs
+  )
   const sessionDay = useSelector(
     (stateSelector: RootState) => stateSelector.workoutModule.sessionDay
   )
 
-  const [editExercise, setEditExercise] = useState(exercise)
-
   useEffect(() => {
-    // console.log('editExercise', editExercise)
+    console.log(exercise)
   }, [exercise])
 
   const onAddSet = async () => {
@@ -51,12 +54,40 @@ export function ExerciseEditor({ exercise }: ExerciseEditorProps) {
       <div className='exercise-editor-container'>
         {sessionDay.sets &&
           sessionDay.sets.length > 0 &&
-          sessionDay.sets.map((set) => {
+          sessionDay.sets.map((set, index) => {
             return (
-              <div key={set._id}>
-                <span>{set.reps}</span>
-                <span>{set.weight}</span>
-              </div>
+              <>
+                <div key={set._id} className='set-editor-container'>
+                  <div className='reps-container'>
+                    <PickerSelect
+                      openClock={() => {}}
+                      option={{
+                        label: 'Reps',
+                        key: 'reps',
+                        type: 'number',
+                      }}
+                      value={set.reps}
+                    />
+                  </div>
+                  <div className='weight-container'>
+                    <PickerSelect
+                      openClock={() => {}}
+                      option={{
+                        label: 'Weight',
+                        key: 'weight',
+                        type: 'number',
+                      }}
+                      value={set.weight}
+                    />
+                  </div>
+                </div>
+
+                {index < sessionDay.sets.length - 1 && (
+                  <Divider
+                    className={`divider ${prefs.isDarkMode ? 'dark-mode' : ''}`}
+                  />
+                )}
+              </>
             )
           })}
         <CustomButton icon={<AddIcon />} onClick={onAddSet} />
