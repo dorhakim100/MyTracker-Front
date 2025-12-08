464f6c9158387774aa9717cd102f17c88ca35ffd
Dor Hakim <dorhakim100@gmail.com>
2025-12-08 20:26:06 +0200

    Add DateRangeController to Progress

--- src/pages/LiftMate/Progress/Progress.tsx
+++ src/pages/LiftMate/Progress/Progress.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
+import { DateRangeController } from '../../../components/DateRangeController/DateRangeController'
+import { getDateFromISO } from '../../../services/util.service'
+import { MONTH_IN_MS } from '../../../assets/config/times'
+
 export function Progress() {
+  const [selectedPastDate, setSelectedPastDate] = useState({
+    from: getDateFromISO(
+      new Date(new Date().getTime() - MONTH_IN_MS).toISOString()
+    ),
+    to: getDateFromISO(new Date().toISOString()),
+  })
+
+  const onDateChange = (dates: { from: string; to: string }) => {
+    setSelectedPastDate(dates)
+  }
+
   return (
-    <div>
-      <h1>Lift Mate Progress</h1>
+    <div className='page-container progress-container'>
+      <DateRangeController
+        selectedPastDate={selectedPastDate}
+        onDateChange={onDateChange}
+      />
     </div>
   )
 }
