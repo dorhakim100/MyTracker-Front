25775421c6512a6796946aecca713897b7afeae9
Dor Hakim <dorhakim100@gmail.com>
2025-12-15 20:37:11 +0200

    Add new basic ExerciseStage

--- src/pages/LiftMate/EditWorkout/NameExercises.tsx
+++ src/pages/LiftMate/EditWorkout/NameExercises.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../../store/store'
 import { CustomInput } from '../../../CustomMui/CustomInput/CustomInput'
@@ -6,6 +7,8 @@ import { Divider, Typography } from '@mui/material'
 import { CustomButton } from '../../../CustomMui/CustomButton/CustomButton'
 import { Dumbbell } from '../../../components/Icons/Dumbbell'
 import AddIcon from '@mui/icons-material/Add'
+import { SlideDialog } from '../../../components/SlideDialog/SlideDialog'
+import { ExercisesStage } from './ExercisesStage'
 
 interface NameExercisesProps {
   workout: Workout
@@ -17,25 +20,40 @@ export function NameExercises({ workout, onNameChange }: NameExercisesProps) {
     (stateSelector: RootState) => stateSelector.systemModule.prefs
   )
 
+  const [addExerciseDialogOpen, setAddExerciseDialogOpen] = useState(false)
+
   return (
-    <div className="edit-workout-stage name-exercises-stage">
-      <CustomInput
-        value={workout.name}
-        onChange={onNameChange}
-        placeholder="Enter workout name"
-        isRemoveIcon={true}
-        className={`${prefs.favoriteColor}`}
+    <>
+      <div className="edit-workout-stage name-exercises-stage">
+        <CustomInput
+          value={workout.name}
+          onChange={onNameChange}
+          placeholder="Enter workout name"
+          isRemoveIcon={true}
+          className={`${prefs.favoriteColor}`}
+        />
+        <Divider className={`divider ${prefs.isDarkMode ? 'dark-mode' : ''}`} />
+        {workout.exercises.length === 0 && (
+          <div className="no-exercises-container">
+            <Dumbbell />
+            <Typography variant="body2">
+              Get started by adding an exercise to your routine
+            </Typography>
+          </div>
+        )}
+        <CustomButton
+          text="Add Exercise"
+          onClick={() => setAddExerciseDialogOpen(true)}
+          icon={<AddIcon />}
+        />
+      </div>
+      <SlideDialog
+        open={addExerciseDialogOpen}
+        onClose={() => setAddExerciseDialogOpen(false)}
+        component={<ExercisesStage />}
+        title="Add Exercises"
+        type="full"
       />
-      <Divider className={`divider ${prefs.isDarkMode ? 'dark-mode' : ''}`} />
-      {workout.exercises.length === 0 && (
-        <div className="no-exercises-container">
-          <Dumbbell />
-          <Typography variant="body2">
-            Get started by adding an exercise to your routine
-          </Typography>
-        </div>
-      )}
-      <CustomButton text="Add Exercise" onClick={() => {}} icon={<AddIcon />} />
-    </div>
+    </>
   )
 }
