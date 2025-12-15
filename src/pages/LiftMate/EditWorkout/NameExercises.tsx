d082a36c9db970a9a19d610ebe867ec1d2602d47
Dor Hakim <dorhakim100@gmail.com>
2025-12-15 20:31:19 +0200

    Add custom dumbbell icon

--- src/pages/LiftMate/EditWorkout/NameExercises.tsx
+++ src/pages/LiftMate/EditWorkout/NameExercises.tsx
@@ -2,6 +2,10 @@ import { useSelector } from 'react-redux'
 import { RootState } from '../../../store/store'
 import { CustomInput } from '../../../CustomMui/CustomInput/CustomInput'
 import { Workout } from '../../../types/workout/Workout'
+import { Divider, Typography } from '@mui/material'
+import { CustomButton } from '../../../CustomMui/CustomButton/CustomButton'
+import { Dumbbell } from '../../../components/Icons/Dumbbell'
+import AddIcon from '@mui/icons-material/Add'
 
 interface NameExercisesProps {
   workout: Workout
@@ -22,6 +26,16 @@ export function NameExercises({ workout, onNameChange }: NameExercisesProps) {
         isRemoveIcon={true}
         className={`${prefs.favoriteColor}`}
       />
+      <Divider className={`divider ${prefs.isDarkMode ? 'dark-mode' : ''}`} />
+      {workout.exercises.length === 0 && (
+        <div className="no-exercises-container">
+          <Dumbbell />
+          <Typography variant="body2">
+            Get started by adding an exercise to your routine
+          </Typography>
+        </div>
+      )}
+      <CustomButton text="Add Exercise" onClick={() => {}} icon={<AddIcon />} />
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   build: {
     outDir: path.resolve(__dirname, '../Back Starter/public'),
     emptyOutDir: true,
