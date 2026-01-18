f161e5970192da7a29f610b48c0d991501f75d6a
Dor Hakim <dorhakim100@gmail.com>
2026-01-18 09:45:36 +0200

    Add PreferencesCard to TrainerSettings

--- src/pages/TrainerDashboard/pages/Settings/TrainerSettings.tsx
+++ src/pages/TrainerDashboard/pages/Settings/TrainerSettings.tsx
@@ -1,14 +1,16 @@
 import { Typography, Box } from '@mui/material'
+import { PreferencesCard } from '../../../../components/PreferencesCard/PreferencesCard'
 
 export function TrainerSettings() {
   return (
-    <Box>
+    <Box className='trainer-settings-container'>
       <Typography variant="h4" gutterBottom>
         Settings
       </Typography>
       <Typography variant="body1" color="text.secondary">
         Manage your trainer account settings here.
       </Typography>
+      <PreferencesCard />
     </Box>
   )
 }
