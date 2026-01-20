5861e71deee3eaed1fc4d558bb4bb75a1dc47a44
Dor Hakim <dorhakim100@gmail.com>
2026-01-20 12:16:12 +0200

    Add Tooltip to EditIcon

--- src/components/EditIcon/EditIcon.tsx
+++ src/components/EditIcon/EditIcon.tsx
@@ -1,5 +1,8 @@
 import IconButton from '@mui/material/IconButton'
 import EditRoundedIcon from '@mui/icons-material/EditRounded'
+import { Tooltip } from '@mui/material'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store/store'
 
 interface EditIconProps {
   onClick?: () => void
@@ -7,16 +10,25 @@ interface EditIconProps {
 }
 
 export function EditIcon({ onClick, ariaLabel = 'Edit' }: EditIconProps) {
+
+
+  const isDashboard = useSelector((state: RootState) => state.systemModule.isDashboard)
+
   function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
     event.stopPropagation()
     onClick?.()
   }
 
+
+
+
   return (
-    <div className='edit-button'>
+
+    <Tooltip title="Edit" className='edit-button' disableHoverListener={!isDashboard}>
       <IconButton onClick={handleClick} aria-label={ariaLabel} size='small'>
         <EditRoundedIcon />
       </IconButton>
-    </div>
+    </Tooltip>
+
   )
 }
