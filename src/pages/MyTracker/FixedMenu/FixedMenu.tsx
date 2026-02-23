b5dd92e9ae9d9de9f89b0197f46e2b7c30bf565c
Dor Hakim <dorhakim100@gmail.com>
2026-02-23 18:34:00 +0200

    Add basic FixedMenu layout and structure

--- src/pages/MyTracker/FixedMenu/FixedMenu.tsx
+++ src/pages/MyTracker/FixedMenu/FixedMenu.tsx
@@ -1,21 +1,88 @@
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
-import { Typography } from '@mui/material'
+import { Divider, Typography } from '@mui/material'
 import { RootState } from '../../../store/store'
+import { CustomButton } from '../../../CustomMui/CustomButton/CustomButton'
+import MenuBookIcon from '@mui/icons-material/MenuBook'
+import { SlideDialog } from '../../../components/SlideDialog/SlideDialog'
+import { LoggedList } from '../../../components/LoggedList/LoggedList'
+
+interface MenuListDialogOptions {
+  open: boolean
+  type: 'menuList' | null
+}
 
 export function FixedMenu() {
   const { t } = useTranslation()
   const prefs = useSelector((state: RootState) => state.systemModule.prefs)
 
+  const [menuListDialogOptions, setMenuListDialogOptions] =
+    useState<MenuListDialogOptions>({
+      open: false,
+      type: null,
+    })
+
+  const getDialogContent = () => {
+    if (!menuListDialogOptions.type) return <></>
+    switch (menuListDialogOptions.type) {
+      case 'menuList':
+        return (
+          <div>
+            <Typography variant='h6'>{t('menu.viewMenus')}</Typography>
+          </div>
+        )
+        break
+
+      default:
+        return <></>
+    }
+  }
+
+  const openMenuListDialog = () => {
+    setMenuListDialogOptions({
+      open: true,
+      type: 'menuList',
+    })
+  }
+
+  const closeMenuListDialog = () => {
+    setMenuListDialogOptions({
+      open: false,
+      type: null,
+    })
+  }
+
   return (
-    <div
-      className={`fixed-menu page-container ${
-        prefs.isDarkMode ? 'dark-mode' : ''
-      }`}
-    >
-      <Typography variant="h6" className="bold-header">
-        {t('prefs.fixedMenu')}
-      </Typography>
-    </div>
+    <>
+      <div
+        className={`fixed-menu page-container ${
+          prefs.isDarkMode ? 'dark-mode' : ''
+        }`}
+      >
+        <div className='fixed-menu-header'>
+          <Typography
+            variant='h5'
+            className='bold-header'
+          >
+            {t('prefs.fixedMenu')}
+          </Typography>
+          <CustomButton
+            text={t('menu.viewMenus')}
+            onClick={openMenuListDialog}
+            icon={<MenuBookIcon />}
+          />
+        </div>
+        <Divider className={`divider ${prefs.isDarkMode ? 'dark-mode' : ''}`} />
+        <LoggedList mealPeriod='breakfast' />
+      </div>
+      <SlideDialog
+        open={menuListDialogOptions.open}
+        onClose={closeMenuListDialog}
+        title={t('menu.viewMenus')}
+        component={getDialogContent()}
+        type='full'
+      />
+    </>
   )
 }
